5730d313203c61594eacbc4bfdb5db8c9c7cc6c9
Erich Welz <erichwelz@gmail.com>
2013-11-04 17:13:49 -0500

    Step 13 finished but edit photos not working

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -2,7 +2,6 @@ class PicturesController < ApplicationController
 
   def index
     @pictures = Picture.all
-    @testing = "surprise"
   end
 
   def show
@@ -25,6 +24,22 @@ class PicturesController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    if @picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{picture.id}"
+    else
+      render :edit
+    end
+  end
+
+
 
   private
   def picture_params
--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -3,9 +3,12 @@ Photogur::Application.routes.draw do
   get 'pictures'=> 'pictures#index'
   
   post 'pictures' => "pictures#create"
-  get 'pictures/new' => 'pictures#new' 
+  get 'pictures/new' => 'pictures#new'
 
-  get 'pictures/:id' => 'pictures#show'
+  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  put 'pictures/:id' => "pictures#update"
+
+  get 'pictures/:id' => 'pictures#show', as: "picture"
  
 end
 
