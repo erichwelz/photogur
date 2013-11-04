6f604d59782d7a6c2ae75c2813410f7d516a522a
Erich Welz <erichwelz@gmail.com>
2013-11-04 17:24:07 -0500

    Step 14 complete - Refactored routes.rb to use resources

--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -1,15 +1,7 @@
 Photogur::Application.routes.draw do
+  resources :pictures
   root :to => 'pictures#index'
-  get 'pictures'=> 'pictures#index'
   
-  post 'pictures' => "pictures#create"
-  get 'pictures/new' => 'pictures#new'
-
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-  put 'pictures/:id' => "pictures#update"
-
-  get 'pictures/:id' => 'pictures#show', as: "picture"
- 
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
