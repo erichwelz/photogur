0b53285761cad3d7c8e7d723f480b9c1ce50ecbd
Erich Welz <erichwelz@gmail.com>
2013-11-04 15:03:45 -0500

    Added html form - fails due to invalid token - will work later in tutorial

--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -1,6 +1,10 @@
 Photogur::Application.routes.draw do
   get 'pictures'=> 'pictures#index'
-  get 'pictures/:id' => 'pictures#show', as: "picture"
+  
+  post 'pictures' => "pictures#create"
+  get 'pictures/new' => 'pictures#new' 
+
+  get 'pictures/:id' => 'pictures#show'
  
 end
 
